43d8e51e3b9ea0442f882d854fbac80a75e71f64
Alex Connolly <D00226250@student.dkit.ie>
2023-01-09 15:36:40 +0000

    WIP - Added Extra Interaction Between PixelCrushers + Inventory
    Added extra interaction between PixelCrushers and the Inventory scripts, wherein, upon collecting a particular item, dialogue variables should change. So far, this desired impact does not function

--- ICA1_DogKnight/Assets/Scripts/InventoryScripts/PickupTrigger.cs
+++ ICA1_DogKnight/Assets/Scripts/InventoryScripts/PickupTrigger.cs
@@ -1,3 +1,4 @@
+using PixelCrushers.DialogueSystem;
 using UnityEngine;
 
 public class PickupTrigger : MonoBehaviour
@@ -16,6 +17,19 @@ public class PickupTrigger : MonoBehaviour
         if(other.tag == activationTag && activationLayer.OnLayer(other.gameObject) && other.gameObject.name.Contains("Player"))
         {
             Inventory.instance.Add(item);
+            if (gameObject.name.Contains("Candle"))
+            {
+                bool hasWax = DialogueLua.GetVariable("HasWax").AsBool;
+                hasWax = true;
+                DialogueLua.SetVariable("HasWax", hasWax);
+            }
+            else if(gameObject.name.Contains("Flower"))
+            {
+                int flowers = DialogueLua.GetVariable("FlowersCollected").AsInt;
+                flowers++;
+                DialogueLua.SetVariable("FlowersCollected", flowers);
+            }
+
             Destroy(gameObject);
         }
     }
